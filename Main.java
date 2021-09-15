61948efb4fbbf3bd7defa42aa801446298de4726
Alex12345678902 <56139315+Phos202@users.noreply.github.com>
2021-09-15 12:53:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,48 @@
+
+
+class Main {
+  public static void main(String[] args) {
+//math calculations in java have a lot of the same operations and they follow the order of operations: + - () / *
+
+int a = 5 + 2 * 7;
+
+    System.out.println(a);
+
+		double b = 2.0 - (10 / 4.0);
+
+		System.out.print(b);
+
+		int c = 10 / 4;
+
+	System.out.print(c);
+
+	double e = 10 / 4;
+
+	System.out.println(e);
+
+	double f = 1.0 * 10 /4;
+
+	System.out.print(f);
+
+	int x = 5;
+
+	x = x + 1; //add 1 to x
+	
+x += 1;
+
+x++; //add 1 to x
+
+System.out.print(x);
+
+x = x - 1;
+
+x -= 1;
+
+x--;
+
+System.out.print(x);
+
+
+
+  }
+}
